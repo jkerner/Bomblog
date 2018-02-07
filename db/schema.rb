8f6864e25342228f14ed19dc8a61d0a4fb40aec5
Julie Kerner <juliek@squareup.com>
2018-02-07 16:10:58 -0500

    remove-and-add-posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180103203108) do
+ActiveRecord::Schema.define(version: 20180207202928) do
 
   create_table "posts", force: :cascade do |t|
-    t.string "name"
     t.string "title"
     t.text "body"
     t.datetime "created_at", null: false
